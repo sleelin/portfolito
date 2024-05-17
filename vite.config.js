b007aac77a7eb1688a3ab1e4174fd954cff87310
Sam Lee-Lindsay <sleelin@gmail.com>
2024-05-17 12:42:21 +1000

    Chore: update vite config for library build

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,11 @@
 import {defineConfig} from "vite";
 import babel from "vite-plugin-babel";
+import CEM from "vite-plugin-cem";
 
 export default defineConfig({
     base: "./",
     build: {
+        minify: false,
         lib: {
             entry: "src/components/index.js",
             formats: ["es"]
@@ -14,9 +16,9 @@ export default defineConfig({
     },
     plugins: [
         babel({
+            include: ["src/**"],
             babelConfig: {
-                babelrc: false,
-                configFile: false,
+                babelrc: false, configFile: false,
                 presets: [
                     ["@babel/preset-env", {modules: false, include: ["@babel/plugin-transform-class-properties"]}]
                 ],
@@ -24,6 +26,10 @@ export default defineConfig({
                     ["@babel/plugin-proposal-decorators", {version: "2023-05"}]
                 ]
             }
+        }),
+        CEM({
+            lit: true,
+            files: ["src/components/**/*.js"]
         })
     ]
 });
