0fca2959f0f9b9c3c4660860e8f2f3d9ea207a40
Sam Lee-Lindsay <sleelin@gmail.com>
2024-05-28 17:12:55 +1000

    Chore(docs): parse demo snippet templates in markdown

--- .vitepress/config/markdown.js
+++ .vitepress/config/markdown.js
@@ -1,5 +1,20 @@
-const renderCode = (md, source, prefix) => md.render("```html\n" + source.replaceAll(prefix, "\n") + "\n```\n")
-    .replace(/(<button.*?><\/button>)/, "");
+const DEMO_PREVIEW_RE = /^<demo(?<attribs>.*?)>(?<previewIndent>\n\s*)(?<preview>.*?)\s*(?:<template #.*?)?<\/demo>$/s;
+const DEMO_SNIPPET_RE = /^<demo.*?>.*?<template #snippet>(?<snippetIndent>\n\s*)(?<snippet>.*?)<\/template>.*?<\/demo>$/s;
+const DEMO_SOURCE_RE = /^<demo.*?>.*?<template #source>(?<sourceIndent>\n\s*)(?<source>.*?)<\/template>.*?<\/demo>$/s;
+const INSERT_PREVIEW_RE = /(?<indent>\s*)\{\{\s*preview\s*}}/s;
+const INSERT_SNIPPET_RE = /(?<indent>\s*)\{\{\s*snippet\s*}}/s;
+
+function renderCode(md, source, prefix) {
+    return md.render("```html\n" + source.replaceAll(prefix, "\n") + "\n```\n")
+        .replace(/(<button.*?><\/button>)/, "");
+}
+
+function insertCode(source, target, matcher, originalIndent, fallbackIndent) {
+    const indent = source?.match(matcher)?.groups?.indent ?? fallbackIndent ?? originalIndent;
+    const insert = `${indent}${target.replaceAll(originalIndent, indent).replaceAll(/\n\s*$/g, "")}`;
+    
+    return source?.replace(matcher, insert) ?? target;
+}
 
 export default (md) => {
     const fallback = md.renderer.rules.html_block;
@@ -9,17 +24,21 @@ export default (md) => {
         const content = token.content.trim();
         
         if (content.startsWith("<demo")) {
-            const matches = content.match(/^<demo(?<attribs>.*?)>(?<previewIndent>\n\s*)(?<preview>.*?)(?:<template #source>(?<sourceIndent>\n\s*)(?<source>.*?)<\/template>)?\s*<\/demo>$/s);
-            const {groups: {attribs, preview, previewIndent, source, sourceIndent}} = matches;
+            const {preview, previewIndent, attribs} = content.match(DEMO_PREVIEW_RE)?.groups ?? {};
+            const {snippet, snippetIndent} = content.match(DEMO_SNIPPET_RE)?.groups ?? {};
+            const {source, sourceIndent} = content.match(DEMO_SOURCE_RE)?.groups ?? {};
+            const snippetCode = insertCode(snippet, preview, INSERT_PREVIEW_RE, previewIndent, snippetIndent);
+            const sourceWithPreview = insertCode(source, preview, INSERT_PREVIEW_RE, previewIndent, sourceIndent);
+            const sourceCode = insertCode(sourceWithPreview, snippet ?? preview, INSERT_SNIPPET_RE, snippetIndent ?? previewIndent, sourceIndent);
             
             return (
                 `<demo${attribs}>\n` +
                     `${preview}\n` +
                     `<template #snippet>\n` +
-                        `${renderCode(md, preview, previewIndent)}\n` +
+                        `${renderCode(md, snippetCode, snippetIndent ?? previewIndent)}\n` +
                     `</template>\n` + (!source ? "" : (
                     `<template #source>\n` +
-                        `${renderCode(md, source, sourceIndent)}\n` +
+                        `${renderCode(md, sourceCode, sourceIndent)}\n` +
                     `</template>\n`)) +
                 `</demo>`
             );
