04093fed373379827fedb6652020856b945b5b49
Sam Lee-Lindsay <sleelin@gmail.com>
2024-06-17 15:34:08 +1000

    Chore(docs): generate outline links from h2 and h3 only

--- .vitepress/config/index.js
+++ .vitepress/config/index.js
@@ -6,7 +6,7 @@ import {head, nav, sidebar} from "./links.js";
 export default defineConfig({
     title: "PortfoLitO", srcDir: "./docs",
     description: "A Lit Web Components Library for building Software Developer Portfolio of Work Pages",
-    head, themeConfig: {logo: "/logo.svg", nav, sidebar, outline: {level: "deep"}},
+    head, themeConfig: {logo: "/logo.svg", nav, sidebar, outline: {level: [2, 3]}},
     markdown: {config: configureMarkdown},
     vite: {
         plugins: vite.plugins,
