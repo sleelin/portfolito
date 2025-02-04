4c8464bd81cde0347a32b6b17a7a9824451fc469
Sam Lee-Lindsay <sleelin@gmail.com>
2025-02-04 16:00:02 +1100

    Add(components/content/hero): handling of absent feature content

--- docs/components/content/hero.md
+++ docs/components/content/hero.md
@@ -3,6 +3,22 @@ import {inject} from "vue";
 const element = inject("manifest").for("content", "hero");
 </script>
 
+<style scoped>
+.demo {
+  content-hero {
+    &::part(container) {
+      height: 500px;
+    }
+    
+    &::part(content) {
+      text-align: left;
+      max-width: 538px;
+      padding: 0;
+    }
+  }
+}
+</style>
+
 # Hero Element
 
 {{ element.summary }}
@@ -11,8 +27,11 @@ const element = inject("manifest").for("content", "hero");
 
 ### Basic Usage
 
-<demo>
+<demo class="scale">
   <content-hero>
+    <img src="/logo.svg" alt="PortfoLitO" slot="image" />
+    <h3>Showcase your Portfolio of Work</h3>
+    <h4>A small library of Lit-based Web Components for Software Developers to build simple Portfolio of Work Pages</h4>
   </content-hero>
 </demo>
 
--- src/components/content/hero.js
+++ src/components/content/hero.js
@@ -1,5 +1,5 @@
 import {LitElement, css, html} from "lit";
-import {customElement} from "lit/decorators.js";
+import {customElement, query, queryAssignedNodes} from "lit/decorators.js";
 
 /**
  * ContentHero element
@@ -10,6 +10,8 @@ import {customElement} from "lit/decorators.js";
  * @slot {<img> | *} image - Image or other content to show in the hero section
  * @slot {*} feature - Articles to feature alongside hero image and tagline
  * @csspart container - Overall responsive container section element
+ * @csspart content - Text and tagline content of the hero element
+ * @csspart feature - Carousel element container for the feature articles of the hero element
  * @cssprop {color} [--container-borderColor=#000000] - Bottom border color of hero section
  * @cssprop {color} [--container-textColor=#F4F4F4] - Foreground text color of tagline text
  * @cssprop {color} [--container-bgColor=#020024] - Primary background color, used as fallback to gradient 
@@ -19,15 +21,25 @@ import {customElement} from "lit/decorators.js";
  */
 @customElement("content-hero")
 export class ContentHero extends LitElement {
+    @query("content-carousel", true)
+    accessor #carousel;
+    
+    @queryAssignedNodes({slot: "feature"})
+    accessor #feature;
+    
+    #slotChange() {
+        this.#carousel.classList.toggle("hidden", !this.#feature.length);
+    }
+    
     render() {
         return html`
-            <section part="container">
+            <section part="container" @slotchange=${this.#slotChange}>
                 <slot name="title"></slot>
                 <slot name="image"></slot>
-                <div class="tagline">
+                <div part="content">
                     <slot></slot>
                 </div>
-                <content-carousel>
+                <content-carousel part="feature">
                     <slot name="feature"></slot>
                 </content-carousel>
             </section>
@@ -46,12 +58,17 @@ export class ContentHero extends LitElement {
             border-bottom: 1px solid var(--container-borderColor, #000000);
             background: var(--container-bgColor, #020024);
             background: linear-gradient(45deg, var(--gradient-fromColor, #020024) 0%, var(--gradient-midColor, #646CFF) 80%, var(--gradient-toColor, #00D4FF) 100%);
-            height: 576px;
             display: grid;
             column-gap: 32px;
             align-items: stretch;
             grid-template-columns: 1fr 512px 1fr;
-            grid-template-areas: "image tagline carousel";
+            grid-template-areas: "image content carousel";
+            
+            &:has(content-carousel.hidden) {
+              justify-content: center;
+              grid-template-columns: auto auto;
+              grid-template-areas: "image content";
+            }
             
             ::slotted([slot=title]) {
               display: none;
@@ -60,25 +77,49 @@ export class ContentHero extends LitElement {
             ::slotted([slot=image]) {
               align-self: center;
               justify-self: end;
-              min-height: 338px;
+              min-height: min(100%, 338px);
               grid-area: image;
             }
             
-            .tagline {
+            [part=content] {
               padding: 0 48px;
-              grid-area: tagline;
+              grid-area: content;
               text-align: center;
               align-content: center;
               color: var(--container-textColor, #F4F4F4);
               animation: 1s ease-in-out fold;
               box-sizing: border-box;
+              display: grid;
+              row-gap: 8px;
+              
+              ::slotted(:is(h2, h3, h4, h5, h6, p)) {
+                margin: 0;
+              }
+              
+              ::slotted(:is(h2, h3, h4, h5, h6)) {
+                line-height: 1.1;
+              }
+              
+              ::slotted(:is(h2, h3)) {
+                font-size: 3em;
+              }
+              
+              ::slotted(:is(h4)) {
+                font-size: 1.5em;
+                font-weight: 500;
+              }
             }
             
             content-carousel {
               min-width: 352px;
               grid-area: carousel;
               
+              &.hidden {
+                display: none;
+              }
+              
               @container (width > 1176px) {
+                padding: 32px 0;
                 container-type: normal;
                 
                 &::part(container) {
@@ -88,10 +129,10 @@ export class ContentHero extends LitElement {
             }
             
             @container (width < 1400px) {
-              grid-template-columns: min-content 1fr max-content;
+              grid-template-columns: min-content auto max-content;
               padding: 0 32px;
               
-              .tagline {
+              [part=content] {
                 padding: 0 32px;
                 max-width: 452px;
                 justify-self: center;
@@ -103,25 +144,30 @@ export class ContentHero extends LitElement {
             }
             
             @container (width <= 1225px) {
-              grid-template-columns: max-content 1fr max-content;
+              grid-template-columns: max-content auto max-content;
             }
             
             @container (width <= 1176px) {
+              padding-top: 32px;
               row-gap: 32px;
-              height: auto;
               min-height: 576px;
               grid-template-rows: min-content 1fr;
               grid-template-columns: repeat(3, 1fr);
               grid-template-areas:
-                "image tagline tagline"
+                "image content content"
                 "carousel carousel carousel";
               
+              &:has(content-carousel.hidden) {
+                grid-template-rows: 1fr;
+                grid-template-columns: auto auto;
+                grid-template-areas: "image content";
+              }
+              
               ::slotted([slot=image]) {
-                align-self: end;
                 justify-self: center;
               }
             
-              .tagline {
+              [part=content] {
                 max-width: 600px;
               }
               
@@ -154,10 +200,10 @@ export class ContentHero extends LitElement {
             }
             
             @container (width <= 976px) {
-              padding: 0 16px;
+              padding: 32px 16px 0;
               column-gap: 16px;
             
-              .tagline {
+              [part=content] {
                 max-width: 480px;
               }
               
@@ -171,18 +217,28 @@ export class ContentHero extends LitElement {
             }
             
             @container (width <= 876px) {
-              height: auto;
               grid-auto-flow: row;
               grid-template-rows: auto min-content 1fr;
               grid-template-columns: 1fr;
               grid-template-areas:
                 "image"
-                "tagline"
+                "content"
                 "carousel";
+              
+              &:has(content-carousel.hidden) {
+                grid-template-rows: auto min-content;
+                grid-template-areas:
+                    "image"
+                    "content";
+              }
+              
+              [part=content] {
+                margin-block: 1em;
+              }
             }
           
             @container (width <= 425px) {
-              .tagline {
+              [part=content] {
                 padding: 0 4px;
               }
             
@@ -225,7 +281,7 @@ export class ContentHero extends LitElement {
                 display: none;
               }
               
-              .tagline {
+              [part=content] {
                 color: inherit;
                 animation: unset;
                 max-width: 50ch;
